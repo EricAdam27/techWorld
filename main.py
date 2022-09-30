c426837f09c6eb7d7ebd0e3eeb0d11a044362f32
Eric Adam <eadamfs.programador@gmail.com>
2022-09-30 16:20:50 -0300

    encapsulate logic with functions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 calculation_to_units = 24
 name_of_unit = 'hours'
 
-print(f'20 days are {20 * calculation_to_units} {name_of_unit}')
-print(f'35 days are {35 * calculation_to_units} {name_of_unit}')
-print(f'50 days are {50 * calculation_to_units} {name_of_unit}')
-print(f'110 days are {110 * calculation_to_units} {name_of_unit}')
+
+def days_to_units(num_of_days, custom_message):
+    print(f'{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}')
+    print(custom_message)
+
+
+days_to_units(20, 'Awesome!')
+days_to_units(35, 'Looks good!')
