213f6857f69aa6c95bfa437856b5da5f40b09195
Eric Adam <eadamfs.programador@gmail.com>
2022-09-30 16:29:04 -0300

    accepting user input

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ calculation_to_units = 24
 name_of_unit = 'hours'
 
 
-def days_to_units(num_of_days, custom_message):
-    print(f'{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}')
-    print(custom_message)
+def days_to_units(num_of_days):
+    return f'{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}'
 
 
-days_to_units(20, 'Awesome!')
-days_to_units(35, 'Looks good!')
+user_input = input('Hey user, enter a number of days and I will convert it to hours!\n')
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
